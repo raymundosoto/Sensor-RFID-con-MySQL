845af60ea9f3b1dc346156e3daba0588dfb32cf0
M. En C. Raymundo Soto <raymundosotosoto@hotmail.com>
2022-10-27 14:24:56 -0500

    Create peticion_query_sql.py

---
+++ peticion_query_sql.py
@@ -0,0 +1,26 @@
+# Bibliotecas
+import mysql.connector
+
+# Conexion
+cnx = mysql.connector.connect(user='usuarioRFID', 
+                              password='RFID', 
+                              host='localhost',
+                              database='codigoIoT')
+
+# Cursor
+cursor = cnx.cursor()
+
+# Query
+query = ("SELECT id,nombre,temperatura,fecha FROM clima WHERE id=555;")
+
+# Ejecutar cursor
+cursor.execute (query)
+
+res = cursor.fetchall ()
+
+for x in res:
+    print (x)
+
+# Cerrar todo
+cursor.close()
+cnx.close()
